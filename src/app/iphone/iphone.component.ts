62fb7eaa9ee2bb3804e4d04d4cbb51deea5095a3
tristan342 <tristanbonal@gmail.com>
2023-01-09 17:08:54 +0100

    first

---
+++ src/app/iphone/iphone.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iphone',
+  templateUrl: './iphone.component.html',
+  styleUrls: ['./iphone.component.scss']
+})
+export class IphoneComponent {
+
+}
